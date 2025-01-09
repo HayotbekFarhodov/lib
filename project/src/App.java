34119bc666d56966994c5feead385da3eaed5359
Hayotbek Farhodov <hayotbekfarhodov020@gmail.com>
2025-01-09 08:03:47 +0500

    feature-b 2month

--- project/src/App.java
+++ project/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args){
-        System.out.println("Открытие магазина через 4дня");
+        System.out.println("Открытие магазина через 2-месяца");
     }
 }
