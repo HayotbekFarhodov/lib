a8cc5af69b0a69ddce51892bc3d4ba6ad01e22e0
Hayotbek Farhodov <hayotbekfarhodov020@gmail.com>
2025-01-09 08:34:34 +0500

    test with reset

--- project/src/App.java
+++ project/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args){
+<<<<<<< HEAD
+        System.out.println("Открытие магазина через 2-месяца");
+=======
+>>>>>>> c1806d4 (feature-a 4days)
         System.out.println("Открытие магазина через 4дня....");
     }
 }
